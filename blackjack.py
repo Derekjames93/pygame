7227a7b3560a9794ab3626f0ffb27d2084a958b7
Pete Looney <petelooney81@gmail.com>
2020-08-27 20:58:36 -0500

    added in the two decks display before dealing

--- blackjack.py
+++ blackjack.py
@@ -7,11 +7,9 @@ class Block(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = pos
 
-class Dealer(Block):
+class Show_Cards(Block):
     pass
 
-class Player(Block):
-    pass
 
 def main():
     width = 400
@@ -23,21 +21,38 @@ def main():
     pygame.display.set_caption('Pete\'s Poker Place')
     clock = pygame.time.Clock()
 
-    # Load dealer image
+    # houses the pictures for the two decks
+    deck = pygame.image.load('images/face_down.png').convert_alpha()
+
+    
     # if dealer face up card == something then, we load the right card
     dealer_card_1 = pygame.image.load('images/heart_A.svg.png').convert_alpha()
     dealer_card_2 = pygame.image.load('images/face_down.png').convert_alpha()
 
+    # show the players cards
+    player_card_1 = pygame.image.load('images/spade_A.svg.png').convert_alpha()
+    player_card_2 = pygame.image.load('images/heart_J.svg.png').convert_alpha()
 
-    # Our dealer
-    dealer_1 = Dealer(dealer_card_1, [200, 100])
-    dealer_group_1 = pygame.sprite.Group()
-    dealer_group_1.add(dealer_1)
 
-    # Our dealer2
-    dealer_2 = Dealer(dealer_card_2, [250, 100])
-    dealer_group_2 = pygame.sprite.Group()
-    dealer_group_2.add(dealer_2)
+    # the two decks for shuffling
+    deck_1 = Show_Cards(deck, [180, 200])
+    deck_2 = Show_Cards(deck, [220, 200])
+    deck_group = pygame.sprite.Group()
+    deck_group.add(deck_1, deck_2)
+
+    # The showing the dealers top card and hiding his second card after
+    # the he has finishing shuffling all the decks
+    dealer_1 = Show_Cards(dealer_card_1, [180, 100])
+    dealer_2 = Show_Cards(dealer_card_2, [220, 100])
+    dealer_card_group = pygame.sprite.Group()
+    dealer_card_group.add(dealer_1, dealer_2)
+
+    # Showing both of the players cards face up
+    player_1 = Show_Cards(player_card_1, [180, 300])
+    player_2 = Show_Cards(player_card_2, [220, 300])
+    player_card_group = pygame.sprite.Group()
+    player_card_group.add(player_1, player_2)
+
     
     # Game initialization
 
@@ -47,20 +62,43 @@ def main():
         for event in pygame.event.get():
 
             # Event handling
-
             if event.type == pygame.QUIT:
                 stop_game = True
 
+            # checks if a key was entered
+            if event.type == pygame.KEYDOWN:
+                # if the Esc key is pressed, then exit the main loop
+                if event.key == pygame.K_ESCAPE:
+                    stop_game = True
 
+                # if the y key is pressed, then we add a card to our user hand
+                if event.key == pygame.K_y:
+                    # print these only after we have shuffled
+                    dealer_card_group.draw(screen)
+                    player_card_group.draw(screen)
+
+
+                # if the n key is pressed, then the we go into the dealers actions
+                if event.key == pygame.K_n:
+                    pass
+        # gets all the keys currently pressed
+        keys = pygame.key.get_pressed()
         # Game logic
         
         # Draw background
         screen.fill(green_color)
 
         # Game display
-        dealer_group_1.draw(screen)
-        dealer_group_2.draw(screen)
-        
+        # pre-shuffled deck
+        deck_group.draw(screen) ## want to add in some sort of shuffle animation that goes before the player and dealer cards are dealt
+
+        # update the cards when a key is pressed
+        # so ideally this would show the cards once the player presses the y button
+        dealer_card_group.update(keys)
+        player_card_group.update(keys)
+
+
+
         pygame.display.update()
         clock.tick(60)
 
